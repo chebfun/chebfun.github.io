4cb067597b8f71ade80fe59726127fa6088d05bc
Hrothgar <hrothgar@maths.ox.ac.uk>
2014-06-23 01:11:38 +0100

    Moved temp/RoseCurves to geom/.

--- examples/geom/RoseCurves.m
+++ examples/geom/RoseCurves.m
@@ -3,17 +3,18 @@
 
 %%
 % (Chebfun example geom/RoseCurves.m)
-% [Tags: #sinewaves, #geometry, #polar]
+% [Tags: #sinewaves, #geometry, #polar, #periodic]
 
 %%
 % A rose curve is a sinusoid in polar coordinates, that is,
 % $$ r = \sin(k\theta). $$
 % Rational values of $k$ produce closed curves, while irrational values
 % produce curves of infinite length that fill the unit disc. If $k = 1$, then
-% the result is a circle.
+% the result is a circle. These figures can be likened to Lissajous curves
+% as in the Example [geom/Lissajous](Lissajous.html) [1].
 
 %%
-% The Wikipedia page about rose curves [1] contains a nice image of many roses
+% The Wikipedia page about rose curves [2] contains a nice image of many roses
 % for different rational values of $k$. This Example reproduces that image.
 
 %%
@@ -21,12 +22,24 @@
 % using Cartesian coordinates:
 % $$ x(t) = \cos(k t) \sin(t), $$
 % $$ y(t) = i \cos(k t) \cos(t). $$
-% Here is a function that creates a
-% chebfun of a rose based on the parameter $k = m/n$ for two integers $m$ and
-% $n$. The domain must be at least as long as $2\pi$ times the least common
-% multiple of $m$ and $n$.
+% Here is a function that creates a chebfun of a rose based on the parameter
+% $k = m/n$ for two integers $m$ and $n$. The domain must be at least as long
+% as $2\pi$ times the least common multiple of $m$ and $n$.
+
 roseCurve = @(m,n) chebfun(@(t) cos(m/n*t).*cos(t)+1i*cos(m/n*t).*sin(t), ...
-                           [0, 2*pi*lcm(m,n)]);
+                           [0, 2*pi*lcm(m,n)], 'periodic');
+
+%%
+% The above command employs the new `'periodic'` flag for Fourier-based
+% chebfuns, which for smooth periodic functions reduces the average number of
+% terms necessary to represent the function by a factor of about $\pi/2$:
+
+m = 50;  n = 51;
+f = roseCurve(m, n);
+g = chebfun(@(x) f(x), [0, 2*pi*lcm(m,n)]);
+length(g) ./ length(f)
+%%
+pi/2
 
 %%
 % Here is the image reproduced.
@@ -66,4 +79,6 @@ time
 
 %% References
 %
-% 1. [https://en.wikipedia.org/wiki/Rose_curve](https://en.wikipedia.org/wiki/Rose_curve)
+% 1. Chebfun Example [geom/Lissajous](Lissajous.html)
+%
+% 2. [https://en.wikipedia.org/wiki/Rose_curve](https://en.wikipedia.org/wiki/Rose_curve)
