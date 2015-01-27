7040cbf6881abeab34ed4251b4173bd91ab6e06e
Hrothgar <hrothgar@maths.ox.ac.uk>
2015-01-27 20:54:03 +0000

    Updated NearestOrthFun and added RandomPolys

--- examples/temp/NearestOrthFun.m
+++ examples/temp/NearestOrthFun.m
@@ -5,11 +5,11 @@
 % (Chebfun Example approx/NearestOrthFun.m)
 % [Tags: #orthogonal polynomials, #quasimatrix, #SVD, #gallery]
 
-function nearestOrthFun()
-%%
+function NearestOrthFun()
+%% Introduction
 % Suppose $A$ is a given $m \times n$ matrix. The problem of finding the
-% orthonormal matrix $Q$ nearest to $A$ is well-known [4] (By an orthonormal
-% matrix, we mean a matrix with orthonormal columns.). Specifically,
+% orthonormal matrix $Q$ nearest to $A$ is well-known [4] (by an orthonormal
+% matrix, we mean a matrix with orthonormal columns). Specifically,
 % it is the following problem:
 %    $$ \mbox{argmin}\_U ||A-U||_\mbox{fro} \quad\mbox{subject to}\quad U^T U = I. $$
 % This problem is a special case of the orthogonal Procrustes problem [3, p. 327].
@@ -21,7 +21,7 @@ function nearestOrthFun()
 %
 % However, the simplest (and possibly a numerically more stable) way is to
 % compute the singular value decomposition of $A$ and to replace the singular
-% values with ones. In other words, if $A = U S V^T$, then
+% values by ones. In other words, if $A = U S V^T$, then
 % $Q = U V^T$ is the nearest orthonormal matrix to $A$. This is
 % actually the unitary factor in the polar decomposition of $A$ [4].
 %
@@ -29,16 +29,16 @@ function nearestOrthFun()
 % Suppose that a set of functions defined on a domain are given.
 % We know how to use Chebfun's `qr` command to compute orthogonal
 % functions (stored as the columns of a qusimatrix `Q2`) corresponding
-% to our given functions. See e.g. [2, Ch. 6] for more details. Here, we
+% to our given functions. See e.g. [2, Ch. 6] for details. Here, we
 % are looking for the nearest set of orthonormal functions. In Chebfun
 % this is also straightforward. We just
 % need to create a quasimatrix out of our given set of functions and use
 % Chebfun's overloaded `svd` command [1]. The function
-% `[Q,Q2] = ortho(A)` in the following computes both `Q` and `Q2`.
+% `[Q,Q2] = nearestOrtho(A)` in the following computes both `Q` and `Q2`.
 
 %% The first six monomials
-A = cheb.gallery('vandermonde'); %A = [1, x, x.^2, x.^3, x.^4, x.^5];
-[Q,Q2] = ortho(A);
+A = cheb.gallery('vandermonde'); % A = [1, x, x.^2, x.^3, x.^4, x.^5];
+[Q,Q2] = nearestOrtho(A);
 
 %%
 % Here, we see Legendre polynomials scaled to have norm 1 (left)
@@ -50,15 +50,15 @@ A = cheb.gallery('vandermonde'); %A = [1, x, x.^2, x.^3, x.^4, x.^5];
 %% Chebyshev-Vandermonde quasimatrix on the interval [0, 1]
 A = cheb.gallery('vandercheb');
 A = restrict(A,[0,1]);
-[Q,Q2] = ortho(A);
-%% Another example
+[Q,Q2] = nearestOrtho(A);
+%% Mixed algebraic and trigonometric example
 x = chebfun('x'); A = [1, cos(x), sin(x.^2), x.^3, x.^4, x.^5];
-[Q,Q2] = ortho(A);
-%% Non-smooth functions on the interval [0, 10]
+[Q,Q2] = nearestOrtho(A);
+%% Non-smooth functions on the interval [0,10]
 A = [cheb.gallery('stegosaurus'), cheb.gallery('wiggly'), cheb.gallery('blasius')];
-[Q,Q2] = ortho(A);
+[Q,Q2] = nearestOrtho(A);
 %%
-function [Q,Q2] = ortho(A)
+function [Q,Q2] = nearestOrtho(A)
 [U,S,V] = svd(A);
 Q = U*V'; % nearest quasimatrix to A with orthonormal columns
 
@@ -83,14 +83,14 @@ end
 
 %% References
 %
-% 1. Z. Battles, L.N. Trefethen, An extension of MATLAB to continuous
-%    functions and operators, SIAM J. Scientific Computing 25 (2004)
+% 1. Z. Battles, and L.N. Trefethen, An extension of MATLAB to continuous
+%    functions and operators, _SIAM J. Scientific Computing_ 25 (2004)
 %    1743-1770.
 %
 % 2. T.A. Driscoll, N. Hale, and L.N. Trefethen, editors, _Chebfun Guide_,
 %    Pafnuty Publications, Oxford, 2014.
 %
-% 3. G.H. Golub, C.F. Van Loan, Matrix Computations, 4th edition, The Johns
+% 3. G.H. Golub, C.F. Van Loan, _Matrix Computations_, 4th edition, The Johns
 %    Hopkins University Press, 2013.
 %
 % 4. N.J. Higham, Matrix nearness problems and applications, pp. 1-27, in
@@ -98,6 +98,6 @@ end
 %    Vol. 22, Oxford Univ. Press, New York, 1989.
 %
 % 5. B.K.P. Horn, H.M. Hilden, S. Negahdaripour, Closed-form solution of
-%    absolute orientation using orthonormal matrices, Journal of the Optical
-%    Society A, 5 (1988) 1127-1135.
+%    absolute orientation using orthonormal matrices, _Journal of the Optical
+%    Society A_, 5 (1988) 1127-1135.
 end
