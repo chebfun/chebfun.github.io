b0f2f20a89e79f2c7f6913c350017f9fc80148bb
Mohsin Javed <mhsnjvd@gmail.com>
2016-12-16 14:56:08 +0000

    updating the carousal

--- demo5.m
+++ demo5.m
@@ -8,14 +8,16 @@ LW = 'linewidth'; MS = 'markersize';
 h = figure;
 
 % Create operator for the Ginzburg-Landau problem
-dom = 20*[-1.2 3.2 -1 1];  tspan = [0 46.5];
-S = spinop2(dom,tspan); S.linearPart = @(u) lap(u);
-S.nonlinearPart = @(u) u - (1+1.5i)*u.*(abs(u).^2);
-% Set an initial condition
+d = 20*[-1.2 3.2 -1 1];  tspan = [0 46.5];
+S = spinop2(d,tspan); S.lin = @(u) lap(u);
+S.nonlin = @(u) u - (1+1.5i)*u.*(abs(u).^2);
+% Set initial condition, solve PDE, plot
 S.init = chebfun2(@(x,y) ...
-    (1i*x+y).*exp(-.1*(x.^2+y.^2)), dom);
-% Solve the PDE and plot
-u1 = spin2(S,'plot','off'); plot(real(u1));
+    (1i*x+y).*exp(-.03*(x.^2+y.^2)), d);
+u = spin2(S, 128, 1e-1, 'plot','off');
+plot(real(u))
+
+
 view(0,90), axis equal, axis off
 
 
