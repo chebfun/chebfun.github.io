d7fde87c89ea9918f6cc57ceaa5f70b7be85c204
Lloyd N. Trefethen <trefethen@maths.ox.ac.uk>
2021-04-23 14:08:19 +0100

    update to ChebopQuiver

--- examples/ode-nonlin/ChebopQuiver.m
+++ examples/ode-nonlin/ChebopQuiver.m
@@ -32,18 +32,18 @@
 % $$  u'' - \mu(1-u^2)u' + u = 0. $$
 %
 % We start by defining a chebop defining the differential equation (selecting
-% $\mu = 3$). Note that we specify the domain of the chebop as well (even
-% though it's not necessary for the quiver plot itself, it will allow us to
-% overlay particular solutions on the quiver plot).
+% $\mu = 3$). Note that we specify the domain of the chebop as well.  This
+% is not necessary for the quiver plot itself, but it will allow us to
+% overlay particular solutions on the quiver plot.
 N = chebop(0,100);
 N.op = @(t,u) diff(u, 2) - 3*(1-u^2)*diff(u) + u;
 %%
-% We call quiver with N as an argument, along with a vector argument that
+% We call |quiver| with N as an argument, along with a vector argument that
 % specifies the lower and upper limits on the $x$ and $y$ axes. Furthermore, we
 % give further arguments for customizing the plot, which are described in
-% detail in the help text of chebop/quiver.
+% detail in the help text of |chebop/quiver|.
 %
-% Once we have called quiver, we overlay the phase plane portraits of solutions
+% Once we have called |quiver|, we overlay the phase plane portraits of solutions
 % obtained by specifying different initial conditions -- notice how the
 % solutions follow the arrows of the quiver plot, then get attracted to the same
 % limit cycle (regardless of whether we start inside or outside of the cycle).
@@ -53,12 +53,11 @@ hold on
 for init = 0.2:0.4:0.2
     N.lbc = [init; 1];
     u = N\0;
-    plot(u, diff(u),'linewidth',2)
+    arrowplot(u, diff(u))
 end
-title('Phase portrait of the van der Pol oscillator','interpreter','latex', ...
-    'fontsize',16)
-xlabel('$u$','interpreter','latex','fontsize',14)
-ylabel('$u''$','interpreter','latex','fontsize',14)
+title('Phase portrait of the van der Pol oscillator')
+IN = 'interpreter'; LT = 'latex';
+xlabel('$u$',IN,LT), ylabel('$u''$',IN,LT)
 hold off
 %% A mathematical pendulum
 %
@@ -67,9 +66,10 @@ hold off
 %
 % $$ u'' + \sin(u) = 0. $$
 %
-% Again we define a chebop, call quiver and overlay solutions for trajectories
+% Again we define a chebop, call |quiver| and overlay solutions on the
+% plot for trajectories
 % starting from the stable equilibrium $u=0$ but with different initial
-% velocities on the plot.
+% velocities.
 N = chebop(0, 50);
 N.op = @(t,u) diff(u,2) + sin(u);
 quiver(N, [-2.5 25 -2 5.5],'xpts',30)
@@ -77,17 +77,15 @@ hold on
 for init = 0:0.5:5
     N.lbc = [0, init];
     u = N\0;
-    plot(u, diff(u),'linewidth',2)
+    arrowplot(u, diff(u))
 end
 hold off
 xlim([-2.5 25])
-title('Phase portrait for an undamped nonlinear pendulum','interpreter','latex', ...
-    'fontsize',16)
-xlabel('$u$','interpreter','latex','fontsize',14)
-ylabel('$u''$','interpreter','latex','fontsize',14)
+title('Phase portrait for an undamped nonlinear pendulum')
+xlabel('$u$',IN,LT), ylabel('$u''$',IN,LT)
 %%
 % We see that for small enough initial velocities, the pendulum swings back and
-% forth around the equilibria $u=0$, while for larger initial velocities, it
+% forth around the equilibrium $u=0$, while for larger initial velocities, it
 % swings over and over the top position. However, if we introduce damping, all
 % trajectories will eventually end up at rest:
 N.op = @(t,u) diff(u,2) + 0.25*diff(u) + sin(u);
@@ -96,15 +94,13 @@ hold on
 for init = 0:0.5:5
     N.lbc = [0, init];
     u = N\0;
-    plot(u, diff(u),'linewidth',2)
+    plot(u, diff(u))
 end
 hold off
-title('Phase portrait for a damped nonlinear pendulum','interpreter','latex', ...
-    'fontsize',16)
-xlabel('$u$','interpreter','latex','fontsize',14)
-ylabel('$u''$','interpreter','latex','fontsize',14)
+title('Phase portrait for a damped nonlinear pendulum')
+xlabel('$u$',IN,LT), ylabel('$u''$',IN,LT)
 %% Lotka-Volterra predator-prey model
-% The final example we consider are the The Lotka-Volterra equations, which
+% The final equations we consider are the The Lotka-Volterra equations, which
 % model the populations of predators (say foxes) and prey (say rabbits) [3].
 % These are a pair of nonlinear, first order differential equations, and exhibit
 % the behaviour that in the absence of predators, the prey population grows
@@ -125,13 +121,11 @@ hold on
 for rabbits = 0.1:.2:1.9
     N.lbc = @(u,v) [u - rabbits; v - 1]; % Initial populations
     [u, v] = N\0;
-    plot(u, v, 'linewidth', 2)
+    arrowplot(u, v)
 end
 hold off
-title('Phase portrait for Lotka-Volterra equations', 'interpreter','latex', ...
-    'fontsize',16)
-xlabel('Rabbits','interpreter','latex','fontsize',14)
-ylabel('Foxes','interpreter','latex','fontsize',14)
+title('Phase portrait for Lotka-Volterra equations')
+xlabel('Rabbits'), ylabel('Foxes')
 %%
 % The cyclical behaviour of the populations is evident. What happens if we
 % increase the reproduction rate of the rabbits by 50%?
@@ -141,22 +135,20 @@ hold on
 for rabbits = 0.1:.2:1.9
     N.lbc = @(u,v) [u - rabbits; v - 1]; % Initial populations
     [u, v] = N\0;
-    plot(u, v, 'linewidth', 2)
+    arrowplot(u, v)
 end
 xlim([0 5]), ylim([0 5])
 hold off
-title('Phase portrait for L-V eqns., increased rabbit reproduction',...
-    'interpreter','latex', 'fontsize',16)
-xlabel('Rabbits','interpreter','latex','fontsize',14)
-ylabel('Foxes','interpreter','latex','fontsize',14)
+title('Phase portrait for L-V eqns., increased rabbit reproduction')
+xlabel('Rabbits'), ylabel('Foxes')
 %%
 % Comparing the phase portraits, we observe that while the maximum rabbit
 % population increased, it was by much less than 50%. In fact, the maximum
 % population of foxes grew more than that of rabbits.
 
 %% References
-% 1. https://en.wikipedia.org/wiki/Phase_portrait
+% 1. http://en.wikipedia.org/wiki/Phase_portrait
 %
-% 2. https://en.wikipedia.org/wiki/Van_der_Pol_oscillator
+% 2. http://en.wikipedia.org/wiki/Van_der_Pol_oscillator
 %
-% 3. https://en.wikipedia.org/wiki/Lotka%E2%80%93Volterra_equations
+% 3. http://en.wikipedia.org/wiki/Lotka%E2%80%93Volterra_equations
